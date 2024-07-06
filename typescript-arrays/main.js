e27a673a27329c82ead8429bd47784eda8e624f8
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-07-06 03:44:44 +0000

    Completed typescript-arrays exercise.

---
+++ typescript-arrays/main.js
@@ -0,0 +1,6 @@
+'use strict';
+const colors = ['red', 'white', 'blue'];
+console.log('value of colors[0] is ', colors[0]);
+console.log('value of colors[1] is ', colors[1]);
+console.log('value of colors[2] is ', colors[2]);
+console.log(`America is ${colors[0]}, ${colors[1]}, and ${colors[2]}.`);
