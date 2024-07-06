c2dd2999a56f947da36b477db25db85a15011b67
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-07-06 22:59:35 +0000

    Completed typescript-data-model exercise.

---
+++ typescript-data-model/order-history.js
@@ -0,0 +1,27 @@
+'use strict';
+const orderHistory = [
+  {
+    items: [
+      { title: 'Gamecube', price: 94.95 },
+      {
+        isbn: '978-1491929483',
+        title:
+          'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+        author: 'Lindsay Bassett',
+      },
+    ],
+  },
+  {
+    items: [{ title: 'Gamecube controller adaptor', price: 15.98 }],
+  },
+  {
+    items: [
+      {
+        isbn: '978-1491929483',
+        title:
+          'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+        author: 'Lindsay Bassett',
+      },
+    ],
+  },
+];
---
+++ typescript-data-model/order-history.ts
@@ -0,0 +1,34 @@
+interface NonBookItem {
+  title: string;
+  price: number;
+}
+
+interface Order {
+  items: (Book | NonBookItem)[];
+}
+const orderHistory: Order[] = [
+  {
+    items: [
+      { title: 'Gamecube', price: 94.95 },
+      {
+        isbn: '978-1491929483',
+        title:
+          'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+        author: 'Lindsay Bassett',
+      },
+    ],
+  },
+  {
+    items: [{ title: 'Gamecube controller adaptor', price: 15.98 }],
+  },
+  {
+    items: [
+      {
+        isbn: '978-1491929483',
+        title:
+          'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+        author: 'Lindsay Bassett',
+      },
+    ],
+  },
+];
