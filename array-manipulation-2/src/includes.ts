c8b05a520835353f7462922ad2e2725b31d9068a
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-07-26 04:00:23 +0000

    df

--- array-manipulation-2/src/includes.ts
+++ array-manipulation-2/src/includes.ts
@@ -1,1 +1,9 @@
 /* exported includes */
+
+function includes(array: unknown[], value: any): boolean {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element === value) return true;
+  }
+  return false;
+}
