df4c166ff68e8b325a1f27237568662abdca4855
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-09-30 17:39:04 +0000

    Completed practice-interview-challenge exercise.

---
+++ practice-interview-challenge/main.js
@@ -0,0 +1,24 @@
+'use strict';
+const getRangeReport = (start, end) => {
+  const evens = [];
+  const odds = [];
+  const range = [];
+  let total = 0;
+  for (let i = start; i <= end; i++) {
+    total += i;
+    let l = odds;
+    if (i % 2 === 0) l = evens;
+    l.push(i);
+    range.push(i);
+  }
+  const average = total / (end - start + 1);
+  return {
+    average,
+    evens,
+    odds,
+    range,
+    total,
+  };
+};
+console.log(getRangeReport(1, 10));
+console.log(getRangeReport(1, 5));
---
+++ practice-interview-challenge/main.ts
@@ -0,0 +1,27 @@
+const getRangeReport: (start: number, end: number) => object = (
+  start: number,
+  end: number
+) => {
+  const evens: number[] = [];
+  const odds: number[] = [];
+  const range: number[] = [];
+  let total = 0;
+  for (let i = start; i <= end; i++) {
+    total += i;
+    let l = odds;
+    if (i % 2 === 0) l = evens;
+    l.push(i);
+    range.push(i);
+  }
+  const average = total / (end - start + 1);
+  return {
+    average,
+    evens,
+    odds,
+    range,
+    total,
+  };
+};
+
+console.log(getRangeReport(1, 10));
+console.log(getRangeReport(1, 5));
